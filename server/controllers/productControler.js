5681059348e25592bce0970cbdaa1452ebb96bd3
Sahil <garg07825@gmail.com>
2023-10-29 15:01:33 +0530

    Added secrets

--- server/controllers/productControler.js
+++ server/controllers/productControler.js
@@ -8,12 +8,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+
 // payment gateay
 const gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+  merchantId: process.env.SECRET_BRAINTREE_MERCHANT_ID,
+  publicKey: process.env.SECRET_BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.SECRET_BRAINTREE_PRIVATE_KEY,
 });
 
 // creating a new Product
