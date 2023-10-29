e0cdc47ea2213a1ded98b5df6a682da0b15bae65
Sahil <garg07825@gmail.com>
2023-10-29 13:53:42 +0530

    public key

--- server/controllers/productControler.js
+++ server/controllers/productControler.js
@@ -12,7 +12,7 @@ dotenv.config();
 const gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
   merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  publicKey: "5xgkcsq2s3z65pfc",
   privateKey: process.env.BRAINTREE_PRIVATE_KEY,
 });
 
